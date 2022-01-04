fb2bf62a6ac10a4bafda8634403080ce10111380
0G1H2O3S4T5H6U7B <97109878+0G1H2O3S4T5H6U7B@users.noreply.github.com>
2022-01-04 17:09:49 +0200

    Update Script-Loader.lua

--- Script-Loader.lua
+++ Script-Loader.lua
@@ -5,7 +5,7 @@ local supportedGameIds = {
 
 if table.find(supportedGameIds, gameId) then
     warn("Game ID: ".. gameId.. " | Player Name: ".. game.Players.LocalPlayer.Name.. " | Game Supported!")
-    if gameId == 155615604 then
+    if gameId == 73885730 then
         loadstring(game:HttpGet("https://raw.githubusercontent.com/0G1H2O3S4T5H6U7B/Scripts/main/Prison-Life.lua"))()
     end
 else
